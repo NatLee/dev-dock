fac8e8b8b950a57983a08dec12fc9957d7a254c4
NatLee <natlee.work@gmail.com>
2023-10-27 17:31:23 +0800

    [update] rm old vnc version

--- scripts/3.install-vnc-core.sh
+++ scripts/3.install-vnc-core.sh
@@ -10,15 +10,8 @@ apt-get -qq purge -y pm-utils xscreensaver*
 echo "Installing TigerVNC server..."
 tar xzf $VNC_TOOL_DIR/tigervnc-1.10.1.x86_64.tar.gz --strip 1 -C /
 
-# noVNC
-echo "Installing noVNC..."
-mkdir -p $NO_VNC_DIR/utils/websockify
-tar xzf $VNC_TOOL_DIR/noVNC-v1.1.0.tar.gz --strip 1 -C $NO_VNC_DIR
-tar xzf $VNC_TOOL_DIR/websockifyv0.9.0.tar.gz --strip 1 -C $NO_VNC_DIR/utils/websockify
-# noVNC's `websockify` needs numpy
-# https://numpy.org/doc/stable/reference/generated/numpy.fromstring.html
-apt-get -y install python-numpy
 
-chmod +x -v $NO_VNC_DIR/utils/*.sh
-# Create index.html to forward automatically to `vnc_auto.html`
+# NoVNC
+git clone --depth 1 https://github.com/novnc/noVNC.git $NO_VNC_DIR
+chmod +x -v $NO_VNC_DIR/utils/*
 ln -s $NO_VNC_DIR/vnc.html $NO_VNC_DIR/index.html
